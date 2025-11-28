f6391f5c5e8d261a2a9ffaab39c345ebb693d3f7
Ahmad Adlan Halim <amdadlhlm@gmail.com>
2025-11-28 18:48:06 +0800

    feat: update embedded map URL to new coordinates

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -5,7 +5,7 @@ import { FaMapMarkedAlt, FaExternalLinkAlt } from 'react-icons/fa';
 
 const Location = () => {
     // Example coordinates (KLCC for demo purposes)
-    const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.796646733236!2d101.71129331475744!3d3.157485397700684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37d12d669c1f%3A0x9e3afdd17c8a9056!2sPetronas%20Twin%20Towers!5e0!3m2!1sen!2smy!4v1645498765432!5m2!1sen!2smy";
+    const mapUrl = "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3970.259376535152!2d100.45981007559219!3d5.675594332434248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwNDAnMzIuMSJOIDEwMMKwMjcnNDQuNiJF!5e0!3m2!1sen!2smy!4v1764326691135!5m2!1sen!2smy"
     const googleMapsLink = "https://goo.gl/maps/4Q8Q8Q8Q8Q8Q8Q8Q8"; // Replace with actual link
 
     return (
